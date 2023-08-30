e4a40f09174f5d75cf994c11f65ad723e3d0c38e
sthj1dx123 <143556405+sthj1dx123@users.noreply.github.com>
2023-08-31 01:26:41 +0800

    Add files via upload

---
+++ env.js
@@ -0,0 +1,27 @@
+window.settings = {
+  // 站点标题
+  title: 'Fast-Lab',
+  // 站点描述
+  description: '世界,本无边界。',
+  // API
+  host: 'https://accnin.fun',
+  // 主题
+  theme: {
+    sidebar: 'light',
+    header: 'dark',
+    color: 'darkblue'
+  },
+  // 背景
+  background_url: '5d891784-174f-48f7-8781-2c31b7ebf0cf',
+  // crisp
+  crisp_id: '',
+  i18n: [
+    'zh-CN',
+    'en-US',
+    'ja-JP',
+    'vi-VN',
+    'ko-KR',
+    'zh-TW',
+    'fa-IR'
+  ]
+}
